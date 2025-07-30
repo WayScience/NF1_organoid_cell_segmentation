d4b2e97025fc376d03c7ef66ddc41d706d6d28cf
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-07-30 12:32:05 -0600

    Updated callbacks to include per-epoch metric evaluation

--- callbacks/Callbacks.py
+++ callbacks/Callbacks.py
@@ -2,85 +2,96 @@ from typing import List, Optional
 
 import mlflow
 import torch
+from torch import Tensor
+from torch.nn import Module
+from torch.utils.data import DataLoader
 
 
 class Callbacks:
     def __init__(
         self,
         metrics: List,
-        loss: torch.nn.Module,
+        loss: Module,
         num_samples_to_plot: Optional[int] = None,
     ):
         self.metrics = metrics
         self.loss = loss
         self.num_samples_to_plot = num_samples_to_plot
+        self.best_loss_value = float("inf")
+        self.early_stopping_counter = 0
 
-    def _log_loss(
-        self, time_step, data_split, predictions, pixel_probs, targets, **kwargs
-    ):
-        loss = self.loss(
-            generated_predictions=predictions,
-            generated_pixel_probs=pixel_probs,
-            targets=targets,
-            data_split=data_split,
-            **kwargs,
-        )
-        for name, value in loss.items():
-            mlflow.log_metric(f"{data_split}_{name}", value, step=time_step)
+    def _log_epoch_metrics(
+        self,
+        time_step: int,
+        model: Module,
+        dataloader: DataLoader,
+        data_split: str,
+        generated_predictions: Tensor,
+        targets: Tensor,
+        **kwargs,
+    ) -> tuple[Tensor, Tensor]:
 
-    def _log_metrics(
-        self, time_step, data_split, predictions, pixel_probs, targets, **kwargs
-    ):
-        for metric in self.metrics:
-            metric_data = metric(
-                generated_predictions=predictions,
-                generated_pixel_probs=pixel_probs,
-                targets=targets,
-                data_split=data_split,
-                **kwargs,
-            )
-            for name, value in metric_data.items():
-                mlflow.log_metric(f"{data_split}_{name}", value, step=time_step)
-
-    def inference(
-        self, model: torch.nn.Module, dataloader: torch.utils.data.DataLoader, **kwargs
-    ):
         model.eval()
-        all_preds, all_targets = [], []
 
         with torch.no_grad():
             for inputs, targets in dataloader:
-                outputs = model(inputs)
-                all_preds.append(outputs)
-                all_targets.append(targets)
+                generated_predictions = model(inputs)
+                pixel_probs = torch.sigmoid(generated_predictions)
 
-        return torch.cat(all_preds), torch.cat(all_targets)
+                for metric in self.metrics:
+                    metric(
+                        generated_predictions=generated_predictions,
+                        generated_pixel_probs=pixel_probs,
+                        targets=targets,
+                        data_split_logging=data_split,
+                        **kwargs,
+                    )
 
-    def _on_epoch_start(self, epoch, **kwargs):
+                self.loss(
+                    generated_predictions=generated_predictions,
+                    generated_pixel_probs=pixel_probs,
+                    targets=targets,
+                    data_split=data_split,
+                    **kwargs,
+                )
+
+        for name, loss_value in self.loss.metric_data().items():
+            mlflow.log_metric(f"{data_split}_{name}", loss_value, step=time_step)
+
+        for metric in self.metrics:
+            for name, metric_value in metric.metric_data().items():
+                mlflow.log_metric(f"{data_split}_{name}", metric_value, step=time_step)
+
+    def _on_epoch_start(self, epoch: int, **kwargs) -> None:
         print(f"Starting epoch {epoch}")
 
-    def _on_batch_start(self, batch, **kwargs):
+    def _on_batch_start(self, batch: int, **kwargs) -> None:
         print(f"Starting batch {batch}")
 
     def _on_epoch_end(
-        self, epoch, generated_predictions, targets, val_dataloader, **kwargs
-    ):
-        train_probs = torch.sigmoid(generated_predictions)
+        self,
+        epoch: int,
+        model: Module,
+        train_dataloader: DataLoader,
+        val_dataloader: DataLoader,
+        **kwargs,
+    ) -> None:
 
-        val_preds, val_targets = self.inference(
-            kwargs["model"], val_dataloader, **kwargs
-        )
-        val_probs = torch.sigmoid(val_preds)
-
-        for split, preds, probs, targs in [
-            ("train", generated_predictions, train_probs, targets),
-            ("validation", val_preds, val_probs, val_targets),
+        for data_split, dataloader in [
+            ("train", train_dataloader),
+            ("validation", val_dataloader),
         ]:
-            self._log_loss(epoch, split, preds, probs, targs, **kwargs)
-            self._log_metrics(epoch, split, preds, probs, targs, **kwargs)
 
-    def _on_batch_end(self, batch, **kwargs):
+            self._log_epoch_metrics(
+                model=model,
+                dataloader=dataloader,
+                data_split=data_split,
+                time_step=epoch,
+                **kwargs,
+            )
+
+    def _on_batch_end(self, batch: int, **kwargs) -> None:
         pass
 
-    def __call__(self, callback_hook, **kwargs):
+    def __call__(self, callback_hook: str, **kwargs) -> None:
         getattr(self, f"_on_{callback_hook}")(**kwargs)
