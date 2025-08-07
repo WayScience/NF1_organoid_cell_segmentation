Updated unet model to ensure correct output and change normalization to mimic unet

models/UNet.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from .UNetLayers import *
+from .UNetLayers import Conv, DoubleConv, OutConv, UpConv
 
 
 class UNet(nn.Module):
@@ -20,75 +20,67 @@ class UNet(nn.Module):
             stride=1,
             padding=1,
             padding_mode="zeros",
+            normalization=nn.BatchNorm2d,
             pooling=None,
         )
         self.inr = Conv(
             in_channels=64,
             out_channels=64,
             kernel_size=3,
-            stride=2,
+            stride=1,
             padding=1,
             padding_mode="zeros",
-            normalization=nn.GroupNorm(num_groups=64 // 4, num_channels=64),
+            normalization=nn.BatchNorm2d,
             pooling=None,
         )
 
-        self.down = {}
-        in_channels = 64
-        next_channels = in_channels * 2
+        self.down = nn.ModuleDict()
+        in_ch = 64
+        out_ch = in_ch * 2
+        encoder_channels = []
 
-        for num_down_layers in range(4):
-            self.down[f"down{num_down_layers}"] = DoubleConv(
-                normalization0=nn.GroupNorm(
-                    num_groups=in_channels // 4, num_channels=in_channels
-                ),
-                normalization1=nn.GroupNorm(
-                    num_groups=next_channels // 4, num_channels=next_channels
-                ),
+        for i in range(4):
+            self.down[f"down{i}"] = DoubleConv(
+                normalization=nn.BatchNorm2d,
                 ascending=False,
-                **{
-                    "in_channels": in_channels,
-                    "out_channels": next_channels,
-                    "kernel_size": 3,
-                    "stride": 1,
-                    "padding": 1,
-                    "padding_mode": "zeros",
-                    "pooling": None,
-                },
+                in_channels=in_ch,
+                out_channels=out_ch,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                padding_mode="zeros",
+                pooling=None,
             )
-            in_channels *= 2
-            next_channels *= 2
+            in_ch = out_ch
+            encoder_channels.append(out_ch)
+            out_ch *= 2
 
         upconv_options = {"upconv": "bilinear", "scale_factor": 2}
 
-        self.up = {}
-        in_channels = 1024
-        next_channels = in_channels / 2
-        for num_up_layers in range(4):
-            self.up[f"up_sample{num_up_layers}"] = UpConv(**upconv_options)
-            self.up[f"up_conv{num_up_layers}"] = self.DoubleConv(
-                normalization0=nn.GroupNorm(
-                    num_groups=in_channels // 4, num_channels=in_channels
-                ),
-                normalization1=nn.GroupNorm(
-                    num_groups=next_channels // 4, num_channels=next_channels
-                ),
+        encoder_channels.sort(reverse=True)
+        self.up = nn.ModuleDict()
+        self.up_convs = nn.ModuleDict()
+
+        for i in range(4):
+            self.up[f"up_sample{i}"] = UpConv(**upconv_options)
+
+        for i, enc_channel in enumerate(encoder_channels):
+            in_ch = enc_channel + (enc_channel // 2)
+            out_ch = enc_channel // 2
+            self.up_convs[f"up_conv{i}"] = DoubleConv(
+                normalization=nn.BatchNorm2d,
                 ascending=True,
-                **{
-                    "in_channels": in_channels,
-                    "out_channels": next_channels,
-                    "kernel_size": 3,
-                    "stride": 1,
-                    "padding": 1,
-                    "padding_mode": "zeros",
-                    "pooling": None,
-                },
+                in_channels=in_ch,
+                out_channels=out_ch,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                padding_mode="zeros",
+                pooling=None,
             )
-            in_channels = next_channels
-            next_channels /= 2
 
         self.outc = OutConv(
-            in_channels=in_channels,
+            in_channels=out_ch,
             out_channels=out_channels,
             kernel_size=1,
             padding=0,
@@ -96,39 +88,24 @@ class UNet(nn.Module):
         )
 
     def forward(self, img: torch.Tensor) -> torch.Tensor:
-        # Height and Width must satisfy to ensure no padding
-        # needs to be added during concatenation:
-        # Height (in pixels) % 2^n = 0, with n levels of downsampling
-        # Width (in pixels) % 2^n = 0, with n levels of downsampling
-        # n = 4 here, so 2^n = 16
-        h, w = img.shape[-2:]
-        pad_h = (16 - h % 16) % 16
-        pad_w = (16 - w % 16) % 16
 
-        pad_left = pad_w // 2
-        pad_right = pad_w - pad_left
-        pad_top = pad_h // 2
-        pad_bottom = pad_h - pad_top
-        pad = (pad_left, pad_right, pad_top, pad_bottom)
-        img = F.pad(img, pad)
+        in0 = self.inr(self.inl(img))  # [B, 64, H/2, W/2]
+        x0 = self.down["down0"](in0)  # [B, 128, H/2, W/2]
+        x1 = self.down["down1"](x0)  # [B, 256, H/4, W/4]
+        x2 = self.down["down2"](x1)  # [B, 512, H/8, W/8]
+        x3 = self.down["down3"](x2)  # [B, 1024, H/16, W/16]
 
-        in0 = self.inr(self.inl(img))
-        x0 = self.down["down0"](in0)
-        x1 = self.down["down1"](x0)
-        x2 = self.down["down2"](x1)
-        x3 = self.down["down3"](x2)
-        xup = self.up["up_conv0"](self.up["up_sample0"](x2, x3))
-        xup = self.up["up_conv1"](self.up["up_sample1"](x1, xup))
-        xup = self.up["up_conv2"](self.up["up_sample2"](x0, xup))
-        xup = self.up["up_conv3"](self.up["up_sample3"](in0, xup))
+        xup = self.up["up_sample0"](x3, x2)
+        xup = self.up_convs["up_conv0"](xup)
+
+        xup = self.up["up_sample1"](xup, x1)
+        xup = self.up_convs["up_conv1"](xup)
+
+        xup = self.up["up_sample2"](xup, x0)
+        xup = self.up_convs["up_conv2"](xup)
+
+        xup = self.up["up_sample3"](xup, in0)
+        xup = self.up_convs["up_conv3"](xup)
+
         logits = self.outc(xup)
-
-        # This will crop out any of the padded sections.
-        # to improve model updates by not considering useless areas (padded areas)
-        if any(pad):
-            left, right, top, bottom = pad
-            logits = logits[
-                ..., top : logits.shape[-2] - bottom, left : logits.shape[-1] - right
-            ]
-
         return logits

models/UNetLayers.py
@@ -27,7 +27,7 @@ class Conv(nn.Module):
             bias=False,
         )
 
-        self.norm = normalization
+        self.norm = normalization(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.pooling = pooling
 
@@ -40,28 +40,45 @@ class Conv(nn.Module):
         return x
 
 
-class DoubleConv:
+class DoubleConv(nn.Module):
     def __init__(
         self,
-        normalization0: nn.Module,
-        normalization1: nn.Module,
+        normalization: nn.Module,
         ascending: bool,
-        **kwargs,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int,
+        padding: int,
+        padding_mode: str = "zeros",
+        pooling: nn.Module = None,
     ):
-        kwargs["normalization"] = normalization0
+        super().__init__()
 
-        self.xl = Conv(**kwargs)
+        self.xl = Conv(
+            in_channels=in_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=stride,
+            padding=padding,
+            padding_mode=padding_mode,
+            normalization=normalization,
+            pooling=pooling,
+        )
 
-        kwargs["in_channels"] = kwargs["out_channels"]
-        kwargs["normalization"] = normalization1
+        self.xr = Conv(
+            in_channels=out_channels,
+            out_channels=out_channels,
+            kernel_size=kernel_size,
+            stride=1 if ascending else 2,
+            padding=padding,
+            padding_mode=padding_mode,
+            normalization=normalization,
+            pooling=None,
+        )
 
-        if not ascending:
-            kwargs["stride"] *= 2
-
-        self.xr = Conv(**kwargs)
-
-    def forward(self):
-        return self.xr(self.xl)
+    def forward(self, tenmap: torch.Tensor) -> torch.Tensor:
+        return self.xr(self.xl(tenmap))
 
 
 class UpConv(nn.Module):
@@ -90,7 +107,7 @@ class UpConv(nn.Module):
     def forward(self, encmap: torch.Tensor, decmap: torch.Tensor) -> torch.Tensor:
         encmap = self.pad_match(encmap, decmap)
         encmap = torch.cat([decmap, encmap], dim=1)
-        return self.up(encmap)
+        return encmap  # Note: return concatenated map; conv is done in DoubleConv
 
 
 class OutConv(nn.Module):
@@ -102,7 +119,7 @@ class OutConv(nn.Module):
         padding: int = 0,
         padding_mode: str = "zeros",
     ):
-        super(OutConv, self).__init__()
+        super().__init__()
         self.conv = nn.Conv2d(
             in_channels=in_channels,
             out_channels=out_channels,