3120fddc1a5e027eb471c822b0f53f0c9a663e51
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-09-05 16:04:56 -0600

    Removed redundant AMP in the post processor

--- datasets/dataset_00/utils/ImagePostProcessor.py
+++ datasets/dataset_00/utils/ImagePostProcessor.py
@@ -11,11 +11,9 @@ class ImagePostProcessor:
     Processes generated predictions computed from the model.
     """
 
-    def __init__(self, device: Union[str, torch.device] = "cuda", use_amp: bool = True):
-        self.device = device
-        self.use_amp = use_amp
+    def __init__(self):
+        pass
 
     def __call__(self, generated_prediction: torch.Tensor) -> torch.Tensor:
 
-        with autocast(enabled=self.use_amp, device_type=self.device):
-            return torch.sigmoid(generated_prediction)
+        return torch.sigmoid(generated_prediction)
