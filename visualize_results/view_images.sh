5bce4a8e4251b0dbac2d3bf6b5a3521f5169b0a9
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-10-14 17:01:35 -0600

    Reference correct uv environment and used more declaration

--- visualize_results/view_images.sh
+++ visualize_results/view_images.sh
@@ -8,20 +8,20 @@ mlflow_artifacts_path=$1
 
 temp_image_dir=$(mktemp -d)
 
-python3 convert_images.py "$mlflow_artifacts_path" "$temp_image_dir"
+uv run python convert_images.py "$mlflow_artifacts_path" "$temp_image_dir"
 
 while true; do
     temp_zarr_dir=$(mktemp -d)
     chosen_dir=$(find "$temp_image_dir" -type f -name "*.tiff" -exec dirname {} \; | sort -u | fzf -x) || exit
     echo "You choose the directory $chosen_dir"
 
-    uvx nviz tiff_to_zarr \
+    uvx --python 3.13 --with nviz==0.0.4 nviz tiff_to_zarr \
         --image_dir "$chosen_dir" \
         --output_path "$temp_zarr_dir/output.zarr" \
-        --channel_map '{"brightfield":"Brightfield","generated_prediction":"Generated Segmentation","TRANS":"Pipeline Segmentation", "thresholded": "Thresholded Generated Segmentation"}' \
+        --channel_map '{"input":"Brightfield","generated-prediction":"Generated Prediction", "segmentation":"Generated Segmentation", "TRANS":"Pipeline Segmentation"}' \
         --scaling_values '(1.0,0.1,0.1)'  # Microscope values
 
-    uvx nviz view_zarr \
+    uvx --python 3.13 --with nviz==0.0.4 nviz view_zarr \
         --zarr_dir "$temp_zarr_dir/output.zarr" \
         --scaling_values '(1.0,0.1,0.1)' \
         --headless "False"
