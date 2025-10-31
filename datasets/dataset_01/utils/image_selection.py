07ff400774ae6074321361bfc92dd5931938bfde
Cameron Mattson <camo@oak.ucdenver.pvt>
2025-10-31 13:51:50 -0600

    Fixed bug when selecting unique whole image indices

--- datasets/dataset_01/utils/image_selection.py
+++ datasets/dataset_01/utils/image_selection.py
@@ -8,16 +8,21 @@ def select_unique_image_idxs(image_dataset_idxs: list[int], image_dataset: datas
     from multiple z-slices from the other dataset.
     """
 
-    unique_image_dataset_idxs = []
+    unique_image_dataset_idxs = set()
+    metadata_idxs = set()
+    len_metadata_idx = 0
     original_split_decision = image_dataset.split_data
     image_dataset.split_data = True
 
     for sample_idx in image_dataset_idxs:
-        if (
-            image_dataset[sample_idx]["metadata"]["Metadata_ID"]
-            not in unique_image_dataset_idxs
-        ):
-            unique_image_dataset_idxs.append(sample_idx)
+        metadata_idxs.add(image_dataset[sample_idx]["metadata"]["Metadata_ID"])
+
+        next_len_metadata_idx = len(metadata_idxs)
+
+        if next_len_metadata_idx > len_metadata_idx:
+            unique_image_dataset_idxs.add(sample_idx)
+
+        len_metadata_idx = next_len_metadata_idx
 
     image_dataset.split_data = original_split_decision
 
